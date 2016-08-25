a45a7f04897bd2723d2fac2ea5add38d6c3371aa
Kamil <kamilpoprawa@vp.pl>
2016-08-25 15:03:22 +0200

    Category enum added to represent available categories. Each category has own description, which may be used for example, to store in the db and then to present it to the user in the frontend.

---
+++ robot-logic/src/main/java/parser/Category.java
@@ -0,0 +1,28 @@
+package parser;
+
+/**
+ * Enum which represents all available categories in the application.
+ * It is possible to return the text (description description) using getDescription() method.
+ */
+public enum Category {
+    EDUCATION_AND_SCIENCE("Education & science"),
+    TRAVEL("Travelling"),
+    LIFESTYLE("Lifestyle & cooking"),
+    SEX("Sex"),
+    MEDICINE("Medicine"),
+    ADVENTURE("Adventure & hobby");
+
+    private final String description;
+
+    Category(String name) {
+        this.description = name;
+    }
+
+    /**
+     * Method to obtain the description of the category.
+     * @return string with the description of particular category.
+     */
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ robot-logic/src/test/java/parser/CategoryTest.java
@@ -0,0 +1,30 @@
+package parser;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static parser.Category.*;
+
+@Test()
+public class CategoryTest {
+    @DataProvider(name = "categoryDescription")
+    private Object[][] testDescriptions() {
+        return new Object[][]{
+                new Object[]{EDUCATION_AND_SCIENCE, "Education & science"},
+                new Object[]{TRAVEL, "Travelling"},
+                new Object[]{LIFESTYLE, "Lifestyle & cooking"},
+                new Object[]{SEX, "Sex"},
+                new Object[]{MEDICINE, "Medicine"},
+                new Object[]{ADVENTURE, "Adventure & hobby"}
+        };
+    }
+
+    @Test(dataProvider = "categoryDescription")
+    public void testCategoryDescriptions(Category category, String description) {
+        // given, when
+        String actualDescription = category.getDescription();
+        // then
+        assertEquals(actualDescription, description);
+    }
+}
