05ac54260a01b08fbad6c800a338e3d92f0dedf3
Daniel <Daniel_Zaucha@epam.com>
2016-08-25 17:32:02 +0200

    work on services

--- robot-service/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
-@EnableAutoConfiguration
-public class Main {
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }}
--- robot-service/src/main/java/TestClass.java
+++
@@ -1,5 +0,0 @@
-public class TestClass {
-    public boolean returnTrue() {
-        return true;
-    }
-}
