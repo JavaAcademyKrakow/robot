6b8ece2958af66edd7483b5df9d6cbe1f856a934
kuba <czyzu0507@gmail.com>
2016-08-31 16:17:45 +0200

    Pagination I introduced method for selecting only given part of information about book from database

--- robot-db/src/main/java/dao/BookDAO.java
+++ robot-db/src/main/java/dao/BookDAO.java
@@ -1,6 +1,8 @@
 package dao;
 
 import domain.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
@@ -11,4 +13,10 @@ import org.springframework.stereotype.Component;
 @Component
 public interface BookDAO extends CrudRepository<Book, Long> {
 
+    /**
+     * Method return selected amount of books for given page
+     * @param pageable pagination information
+     * @return List of books
+     */
+    Page<Book> FindAll(Pageable pageable);
 }
