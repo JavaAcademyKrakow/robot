046ba7079db2fcf3002149d6d07c65151705d3be
Kamil <kamilpoprawa@vp.pl>
2016-09-06 10:06:48 +0200

    Simple cleaning and fixing. Unuused imports removed. Some javadocs added.

--- robot-db/src/main/java/dao/BookDAO.java
+++ robot-db/src/main/java/dao/BookDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 import domain.Book;
-import domain.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
