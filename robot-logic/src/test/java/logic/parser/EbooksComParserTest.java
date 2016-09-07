6618c6ba119b00fa3e43033ec72cda3183075c93
Kamil <kamilpoprawa@vp.pl>
2016-09-07 12:51:37 +0200

    Tests for EbooksComParser refactored. New tests added. Now, three different cases are covered: 	1. the web page does not contain any books 	2. the web page does not contain books on discount 	3. the web page contains free books or books on discount In tests samples of ebooks.com web pages are used. They are stored in test/resources directory.

--- robot-logic/src/test/java/logic/parser/EbooksComParserTest.java
+++ robot-logic/src/test/java/logic/parser/EbooksComParserTest.java
@@ -1,8 +1,8 @@
 package logic.parser;
 
 
-import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.testng.annotations.Test;
 import repositories.ParsedBook;
 
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 /**
@@ -22,23 +23,80 @@ import static org.testng.Assert.assertTrue;
 public class EbooksComParserTest {
 
     /**
-     * Simple test for EbooksComParser - it uses one html file captured from the website
+     * This test covers the case in which parser tries to parse empty web page.
+     * In this case, empty Optional should be returned.
+     *
+     * @throws IOException - when classloader has problems with finding html file in resources directory.
+     */
+    @Test
+    public void testEmptyDocument() throws IOException {
+        // given
+        final String emptyWebPage = "<html><head><title>Empty doc</title></head><body>There are no books in here!</body></html>";
+        final EbooksComParser parser = mock(EbooksComParser.class);
+
+        // when
+        when(parser.openDocument()).thenReturn(Jsoup.parse(emptyWebPage));
+        when(parser.parse()).thenCallRealMethod();
+        Optional<List<ParsedBook>> resultList = parser.parse();
+
+        // then
+        assertFalse(resultList.isPresent());
+    }
+
+    /**
+     * In this case the document does not have any free books or books on discount.
+     * Expected result of parsing is an empty list.
+     *
+     * @throws IOException - when classloader has problems with finding html file in resources directory.
+     */
+    @Test
+    public void testDocumentWithoutBooksOnDiscount() throws IOException {
+        // given
+        InputStream is = ClassLoader.getSystemResourceAsStream("ebooks_com/no_discount.html");
+        EbooksComParser parser = mock(EbooksComParser.class);
+        Document doc = Jsoup.parse(is, null, "http://www.ebooks.com/subjects/education/");
+
+        // when
+        when(parser.openDocument()).thenReturn(doc);
+        when(parser.parse()).thenCallRealMethod();
+        Optional<List<ParsedBook>> resultList = parser.parse();
+        List<ParsedBook> unpackedList;
+
+        if (resultList.isPresent())
+            unpackedList = resultList.get();
+        else
+            unpackedList = null;
+
+        // then
+        assertTrue(unpackedList != null && unpackedList.isEmpty());
+    }
+
+
+    /**
+     * This method performs scenario in which some books on the discount are found. It uses html retrieved
+     * from one of the web pages from ebooks.com. To simplify the test, only the number of books found are checked.
      *
      * @throws IOException - when classloader has problems with finding html file in resources directory.
      */
     @Test
     public void testParser() throws IOException {
         // given
-        InputStream is = ClassLoader.getSystemResourceAsStream("education_example.html");
-        String html = IOUtils.toString(is);
+        InputStream is = ClassLoader.getSystemResourceAsStream("ebooks_com/with_discount.html");
         EbooksComParser parser = mock(EbooksComParser.class);
+        Document doc = Jsoup.parse(is, null, "http://www.ebooks.com/subjects/education/");
 
         // when
-        when(parser.openDocument()).thenReturn(Jsoup.parse(html));
+        when(parser.openDocument()).thenReturn(doc);
         when(parser.parse()).thenCallRealMethod();
         Optional<List<ParsedBook>> optionalListOfBooks = parser.parse();
+        List<ParsedBook> unpackedList;
+
+        if (optionalListOfBooks.isPresent())
+            unpackedList = optionalListOfBooks.get();
+        else
+            unpackedList = null;
 
         // then
-        assertTrue(optionalListOfBooks.get().size() == 8);
+        assertTrue(unpackedList != null && unpackedList.size() == 8);
     }
 }
