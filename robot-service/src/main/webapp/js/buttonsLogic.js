42719ffdba6ac0e5dc268b3e6fbda9365e40b3f4
Daniel <Daniel_Zaucha@epam.com>
2016-09-07 16:49:31 +0200

    Bug fixing. All &nbsp are removed. Properties.js is removed

--- robot-service/src/main/webapp/js/buttonsLogic.js
+++ robot-service/src/main/webapp/js/buttonsLogic.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $('#refresh').click(function(){
     $.ajax({
         method: "GET"
-        , url: "http://localhost:8090/reset"
+        , url: "http://localhost:8080/reset"
         , dataType: 'jsonp'
         , jsonpCallback: "myFunc",
         success: function (data) {
